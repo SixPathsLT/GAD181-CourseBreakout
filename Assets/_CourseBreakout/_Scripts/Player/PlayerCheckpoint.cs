77abc11883ee7302ccf69eacdd31aa8a8e4cecd8
SixPathsLT <wavesgamingllc@gmail.com>
2021-07-22 03:26:18 +0400

    Update 1.4.7 - HOT FIX Major issue of player not respawning
    Resolved issue with checkpoint script not being able to respawn player in the build.

--- Assets/_CourseBreakout/_Scripts/Player/PlayerCheckpoint.cs
+++ Assets/_CourseBreakout/_Scripts/Player/PlayerCheckpoint.cs
@@ -18,7 +18,7 @@ public class PlayerCheckpoint : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
+    { 
         spawnPoint = gameObject.transform.position;
     }
 
